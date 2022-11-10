e84c5bfb4e59683f9f2b2f81b4e939f50fc5e752
christinej604 <77814489+christinej604@users.noreply.github.com>
2022-11-10 13:29:46 -0500

    finished studio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,33 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  let total = 0.00;
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>My Bakery</h1> 
+      {bakeryData.map((item, index) => (
+       <BakeryItem 
+       index={index}
+       name={item.name} 
+       image={item.image} 
+       description={item.description} 
+       price={item.price}
+       setCart={setCart}
+       cart={cart}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+       ></BakeryItem>
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {cart.map(({name, price, quantity}) => {
+          total += price * quantity
+          return (<p>{quantity} x {name}: ${price} </p>);
+        })}
+        <p>Total: $ {total}</p>
+        
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,28 @@
-// TODO: create a component that displays a single bakery item
+import "./BakeryItem.css"
+export default function BakeryItem(props) {
+	function handleClick() {
+    const itemInCart = {name: props.name, price: props.price, quantity: 1};
+    const itemExistInCart = props.cart.find((element) => element.name === props.name);
+    if (itemExistInCart !== undefined) {
+      props.setCart(
+        props.cart.map((element) => {
+          if (element === itemExistInCart) {
+            return {name: props.name, price: props.price, quantity: element.quantity + 1};
+          } else {
+            return element;
+          }
+        })
+      );
+    } else {
+      props.setCart(props.cart.concat(itemInCart));
+    }
+  };
+  return (
+		 <div>
+        <h2> {props.name}</h2>
+        <img src={props.image} alt={props.description}/>
+        <p> Price: ${props.price}</p>
+        <button onClick={handleClick}>Add to Cart</button>
+      </div>
+	);
+}
